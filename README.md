6b36ea630bc0f6ce74d28bf135702d589ad0750e
Tzu-Ti <a2699560@gmail.com>
2025-09-11 14:22:28 +0800

    fix the split data code, and create the json files

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # WiFi2Seg
+
+## Split Dataset
+### split dataset to train and validation
+```shell=
+$ python3 split_data.py --raw_data_root /root/bindingvolume/CSI_UNCC/raw --glob_path env*/[FM][12]/*/* --mode split --name "train&val" --ratio 9:1
+```
+The json file will be like:
+```
+{
+    'train': [xxx.npz, xxx.npz, xxx.npz],
+    'val': [xxx.npz, xxx.npz, xxx.npz]
+}
+```
+### dataset to test.json or val.json
+```shell=
+$ python3 split_data.py --raw_data_root /root/bindingvolume/CSI_UNCC/raw --glob_path test_set/[FM][12]/*/* --mode whole --name "test"
+```
+The json file will be like:
+```
+{
+    'data': [xxx.npz, xxx.npz]
+}
+```
--- datas/split_data.py
+++ datas/split_data.py
@@ -3,6 +3,7 @@ import glob
 import os
 import random
 import json
+from tqdm import tqdm
 
 def parse_args():
     """
@@ -12,57 +13,59 @@ def parse_args():
     :param mode: train (or val, test. train -> train&val.json, val -> val.json, test -> test.json)
     """
     parser = argparse.ArgumentParser(description="Split data script")
-    parser.add_argument('--data_root', type=str, required=True, help='Root directory of the data')
-    parser.add_argument('--person_number', type=int, required=True, help='Number of people')
-    parser.add_argument('--mode', type=str, default='train', help='Dataset mode')
+    parser.add_argument('--raw_data_root', required=True, help='Root directory of the raw data')
+    parser.add_argument('--glob_path', required=True, help='Glob path to find the data')
+    parser.add_argument('--mode', required=True, choices=['split', 'whole'], help='split or whole dataset')
+    parser.add_argument('--name', required=True, help='Name of the dataset')
+    parser.add_argument('--ratio', default="8:2", help='Train/Val split ratio')
     return parser.parse_args()
 
 def main():
     args = parse_args()
-    print(f"Data root directory: {args.data_root}")
-    print(f"Number of people: {args.person_number}")
-    print(f"Dataset mode: {args.mode}")
+    print(f"Data root directory: {args.raw_data_root}")
+    print(f"Glob path: {args.glob_path}")
+    print(f"Mode: {args.mode}")
+    if args.mode == 'split':
+        print(f"Split ratio: {args.ratio}")
 
-    if args.person_number == 1:
-        codes = ['F1', 'F2', 'F3', 'M1', 'M2', 'M3', 'N']
-
-    # get the json file name
-    if args.mode == 'train':
-        json_name = 'train&val.json'
-    elif args.mode == 'val':
-        json_name = 'val.json'
-    elif args.mode == 'test':
-        json_name = 'test.json'
+    dataset_name = f"{args.name}.json"
+    print(f"Dataset name: {dataset_name}")
 
     # get all file path
-    all_npy_paths = []
-    for c in codes:
-        if args.mode == 'train':
-            c_np_paths = glob.glob(os.path.join(args.data_root, 'Env*', 'npy', c, '*', '*', '*.npz'))
-        elif args.mode == 'val':
-            c_np_paths = glob.glob(os.path.join(args.data_root, 'val_set', 'npy', c, '*', '*', '*.npz'))
-        elif args.mode == 'test':
-            c_np_paths = glob.glob(os.path.join(args.data_root, 'test_set', 'npy', c, '*', '*', '*.npz'))
-        all_npy_paths += c_np_paths
-    print(f"Total number of npz files: {len(all_npy_paths)}")
+    all_csi_paths = []
+    rgb_paths = glob.glob(os.path.join(args.raw_data_root, 'rgb', args.glob_path, '*.jpg'))
+    print(f"Total number of RGB files found: {len(rgb_paths)}")
 
-    # shuffle and split data, if mode is train, split into train and val
-    if args.mode == 'train':
-        random.shuffle(all_npy_paths)
-        split_ratio = 0.8
-        split_idx = int(len(all_npy_paths) * split_ratio)
-        datas = {}
-        datas['train'] = all_npy_paths[:split_idx]
-        datas['val'] = all_npy_paths[split_idx:]
+    # check if CSI corresponding npy file exists
+    for rgb_path in tqdm(rgb_paths):
+        csi0_path = rgb_path.replace('raw', 'parsed').replace('rgb', 'csi0').replace('.jpg', '.npz')
+        csi1_path = csi0_path.replace('csi0', 'csi1')
+        csi2_path = csi0_path.replace('csi0', 'csi2')
+        if os.path.exists(csi0_path) and os.path.exists(csi1_path) and os.path.exists(csi2_path):
+            all_csi_paths.append(csi0_path)  # just need one of the csi files
+    length = len(all_csi_paths)
+    print(f"Total number of complete CSI sets found: {length}")
+
+    if args.mode == 'split':
+        ratio0, ratio1 = map(int, args.ratio.split(':'))
+        assert ratio0 + ratio1 == 10, "The sum of the split ratio must be 10"
+        split_idx = int(length * ratio0 / (ratio0 + ratio1))
+
+        # shuffle and save to json
+        random.shuffle(all_csi_paths)
+        datas = {
+            'train': all_csi_paths[:split_idx],
+            'val': all_csi_paths[split_idx:]
+        }
         print(f"Number of training data: {len(datas['train'])}")
         print(f"Number of validation data: {len(datas['val'])}")
-    else:
-        datas = {}
-        datas[args.mode] = all_npy_paths
-        print(f"Number of {args.mode} data: {len(datas[args.mode])}")
+    elif args.mode == 'whole':
+        datas = {
+            'data': all_csi_paths
+        }
+        print(f"Number of data: {len(datas['data'])}")
 
-    # write to a json file
-    with open(json_name, 'w') as f:
+    with open(dataset_name, 'w') as f:
         json.dump(datas, f)
 
 if __name__ == "__main__":
